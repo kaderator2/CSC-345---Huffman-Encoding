367c3a8ea1f6e2d42e15f0d1560e4c163dd66a87
Kade Dean <kadehdean@gmail.com>
2024-04-06 11:28:27 -0700

    Implemented gradle for testing

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+
+// This class acts as the main driver code to run and implement all our
+// other written java files and output the huffman encoding data.
+//
+// Authors: Kade Dean
+
+public class Main {
+    public static void main(String[] args) {
+        // FileRead fileRead = new FileRead();
+        // String filename = fileRead.getFilename();
+        HuffmanTree hTree = new HuffmanTree();
+        System.out.println("Making new tree!");
+        hTree = hTree.constructTree("testFile.txt");
+
+        System.out.println("new tree made!\nStarting to encode file!");
+        hTree.encodeFile("testFile.txt", "encodedFile.txt");
+        System.out.println("Encoded File!");
+        hTree.decodeFile("encodeFile.txt");
+        System.out.println("Decoded File!");
+    }
+}
