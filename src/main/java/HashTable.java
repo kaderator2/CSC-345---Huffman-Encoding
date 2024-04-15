48c07b6946d36fd0719024123c7c9ea20bcddf52
Ethan D Quimpo <143456231+Maganca@users.noreply.github.com>
2024-04-15 16:54:22 -0700

    Update HashTable.java
    major overhauls, debugged resize

--- src/main/java/HashTable.java
+++ src/main/java/HashTable.java
@@ -1,4 +1,3 @@
-package src.main.java;
 
 // Implements the functionality of a HashTable, which will be used to store the 
 // frequencies of different characters within a given file. In other words, the
@@ -36,29 +35,28 @@ public class HashTable {
      *
      * @return int size of the hashtable.
      */
-    public static int size() {
+    public  int getNumElements() {
         return numElements;
     }
-
+    public int  getSize() {
+    	return size;
+    }
     private int hash(char givenCharacter) {
         return (Character.hashCode(givenCharacter) & 0x7fffffff) % size;
     }
-
+    
     // Wrapper method for actually adding to the hashtable
     public void add(char givenCharacter, String encoding) {
-        insert(givenCharacter, encoding, table);
-
-        // Resizes the table up, if necessary
-        if ((numElements * 1.0) / table.length > 0.5) {
-            resize();
-        }
+    	resize();// Resizes the table up, if necessary
+        insert(givenCharacter, encoding, table, true);
     }
+   
 
     // Method for adding a character to an underlying array. Note that if the
     // character
     // already exists, this should simply increment the count, and that if the table
     // becomes more than half full, then we resize
-    private void insert(char givenCharacter, String encoding, HashNode[] givenArray) {
+    private void insert(char givenCharacter, String encoding, HashNode[] givenArray, boolean isReInsert) {
         int hash = hash(givenCharacter);
 
         // Loops through the hash table
@@ -68,7 +66,9 @@ public class HashTable {
             if (givenArray[hash] == null) {
                 givenArray[hash] = new HashNode(givenCharacter);
                 givenArray[hash].setEncoding(encoding);
-                numElements++;
+                if (isReInsert) {
+                	numElements++;
+                }
                 return;
             }
 
@@ -88,7 +88,9 @@ public class HashTable {
                         // If we find an empty position, we just replace the tombstone
                         givenArray[hash] = new HashNode(givenCharacter);
                         givenArray[hash].setEncoding(encoding);
-                        numElements++;
+                        if (isReInsert) {
+                        	numElements++;
+                        }
                         return;
                     }
 
@@ -106,39 +108,7 @@ public class HashTable {
             hash = (hash + 1) % givenArray.length;
         }
 
-        /*
-         * if (table[index] == null || table[index].getTombstone()) {
-         * // Space is available, insert a new HashNode
-         * table[index] = new HashNode(givenCharacter);
-         * numElements++;
-         * } else if (table[index].getChar() == givenCharacter) {
-         * // Character already exists, increment the count
-         * table[index].incrementCount();
-         * } else {
-         * int probeCount = 1;
-         * int probeIndex = (index + probeCount) % size;
-         * 
-         * while (probeCount < size) {
-         * if (table[probeIndex] == null || table[probeIndex].getTombstone()) {
-         * // Empty slot or tombstone found, insert a new HashNode
-         * table[probeIndex] = new HashNode(givenCharacter);
-         * numElements++;
-         * break;
-         * } else if (table[probeIndex].getChar() == givenCharacter) {
-         * // Character already exists, increment the count
-         * table[probeIndex].incrementCount();
-         * break;
-         * } else {
-         * // Slot is occupied by a different character, probe to the next slot
-         * probeCount++;
-         * probeIndex = (index + probeCount) % size;
-         * }
-         * }
-         * }
-         * if (numElements > (size / 2)) {
-         * resize();
-         * }
-         */
+     
 
     }
 
@@ -168,19 +138,37 @@ public class HashTable {
     // private method used for resizing,
     // doubles the size of the table while preserving the indices.
     private void resize() {
-        int newSize = (size * 6) - 1;
-        HashNode[] newTable = new HashNode[newSize];
+        if (((numElements * 1.0) / size) > 0.5) {
+            System.out.print(numElements + "\n");
+            int newSize = (size * 2);
+            HashNode[] newTable = new HashNode[newSize];
 
-        // Transfer all the elements to the new table, rehashing them as we do so
-        for (int i = 0; i < size; i++) {
-            HashNode node = table[i];
-            if (node != null && node.getTombstone() != true) {
-                insert(node.getChar(), node.getEncoding(), newTable);
+            // Transfer all the elements to the new table, rehashing them as we do so
+            for (int i = 0; i < size; i++) {
+                HashNode node = table[i];
+                if (node != null && !node.getTombstone()) {
+                    // Rehash the element and insert it into the new table
+                    int newHash = (Character.hashCode(node.getChar()) & 0x7fffffff) % newSize;
+                    int numProbes = 0;
+                    while (newTable[newHash] != null) {
+                        newHash = (newHash + 1) % newSize;
+                        numProbes++;
+                        if (numProbes >= newSize) {
+                            // If we've probed all the slots and the table is full,
+                            // resize again to prevent an infinite loop.
+                            size = newSize;
+                            table = newTable;
+                            resize();
+                            return;
+                        }
+                    }
+                    newTable[newHash] = node;
+                }
             }
-        }
 
-        table = newTable;
-        size = newSize;
+            table = newTable;
+            size = newSize;
+        }
     }
 
     // debugging function used to print out the nodes
@@ -206,19 +194,23 @@ public class HashTable {
     // for value,
     // returns -1 if not found
     public int getCount(char givenChar) {
-        int index = hash(givenChar);
-        if (table[index].getChar() == givenChar) {
-            return table[index].getCount();
-        } else {
-            while (index <= size) {
-                if (table[index].getChar() == givenChar) {
-                    return table[index].getCount();
-                }
-                index = (index + 1) % table.length;
+        int hash = hash(givenChar);
+        
+        HashNode node = table[hash];
+        
+        while (node != null) {
+            if (node.getChar() == givenChar && !node.getTombstone()) {
+                // Character found, return its count
+                return node.getCount();
             }
+            
+            // Move to the next slot in the hash table
+            hash = (hash + 1) % size;
+            node = table[hash];
         }
+        
+        // Character not found
         return -1;
-
     }
 
     // confirms if specific index is null
@@ -233,11 +225,19 @@ public class HashTable {
     public int getFrequency(int index) {
         return table[index].getCount();
     }
+   
 
     public static void main(String[] args) {
         HashTable testHash = new HashTable();
-        testHash.add('p', "");
-        testHash.add('e', "");
+        String characters = "abcdefghijklmnopqrstuvwxyz";
+        
+        
+        for (char c : characters.toCharArray()) {
+        	
+            testHash.add(c, "");
+        }
+        testHash.add('a', "");
+        testHash.add('b', "");
         testHash.add('e', "");
         testHash.add('i', "");
         testHash.add('n', "");
@@ -246,31 +246,8 @@ public class HashTable {
         System.out.print(testHash.getCount('e') + "\n");
         System.out.print(testHash.isNull(7) + "\n");
         System.out.print(testHash.isNull(8) + "\n");
-        System.out.print(testHash.getChar(8) + "\n");
+        System.out.print(testHash.getChar(40) + "\n");
     }
 }
 
-/**
- * public void add(char givenCharacter) { int index = contains(givenCharacter,
- * table); if(index != -1) { table[index].count++;
- * 
- * } else{ HashNode insert = new HashNode(givenCharacter); table[numElements] =
- * insert; numElements++;
- * 
- * } //replace the og table with a new table double the size and with the same
- * values. if (numElements >= (size/2)) { HashNode[] newTable = new
- * HashNode[size*2]; for (int j = 0; j < numElements;j++) { newTable[j] =
- * table[j]; } table = newTable; } } //returns character at given index public
- * char getChar(int index) { return table[index].nodeChar; } public int
- * getFrequency(int index) { return table[index].count; }
- * 
- * // Method that checks to see if a target character is within a HashTable. //
- * returns the index of the target character or -1 if index is not found.
- * private int contains(char target, HashNode[] table) { for(int i = 0; i <
- * numElements;i++) { if (table[i].nodeChar == target) { return i; } } return
- * -1; } // debugging method that prints the contents of the table public static
- * void printTable() {
- * 
- * for (int k = 0; k < numElements;k++) { System.out.print(table[k].nodeChar + "
- * "); } System.out.print("\n"); }
- */
+
